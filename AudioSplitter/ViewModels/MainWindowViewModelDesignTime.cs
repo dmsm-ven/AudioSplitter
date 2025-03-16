3d2d459e732b4393c48647996648134820c0a928
Painven <painven@gmail.com>
2025-03-16 18:07:15 +0300

    обновлены control styles

--- AudioSplitter/ViewModels/MainWindowViewModelDesignTime.cs
+++ AudioSplitter/ViewModels/MainWindowViewModelDesignTime.cs
@@ -6,14 +6,15 @@ public partial class MainWindowViewModelDesignTime : MainWindowViewModel
 {
     public MainWindowViewModelDesignTime() : base(null, null)
     {
-        Enumerable.Range(1, 9).Select(i => new AudioFileChunkDisplayItem()
+        Enumerable.Range(1, 4).Select(i => new AudioFileChunkDisplayItem()
         {
             AlbumName = $"Test album name",
             ArtistName = $"Test artist name",
-            TrackName = $"TEST NAME #{i}",
+            TrackName = $"DESIGN TIME NAME {i}",
             TimeStart = TimeSpan.FromSeconds(0 + (i - 1) * 60),
             TimeEnd = TimeSpan.FromSeconds(60 + (i - 1) * 60),
-            TrackNumber = i
+            TrackNumber = i,
+            LengthInBytes = 1_000_000 * i
         }).ToList().ForEach(item =>
         {
             this.ChunkItems.Add(item);
